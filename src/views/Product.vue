cdfce45ea4469279fc0d7a33bdfb81dc5c160c30
sonora-mellow <>
2018-08-24 18:25:04 +0900

    add product-table

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="product">
   	<h1>商品情報</h1>
-    <p>このページは ID. {{ id }} の詳細を表示する</p>
+    <dl class="product-table">
+      <dt>商品名</dt><dd>{{ item.name }}</dd>
+      <dt>価格</dt><dd>{{ item.price }}円</dd>
+      <dt>商品説明</dt><dd>{{ item.content }}</dd>      
+    </dl>
   </div>
 </template>
 
@@ -11,8 +15,17 @@ export default {
   props: { id: Number },
   data() {
     return {
-
+      item: null
     };
+  },
+  watch {
+    id: {
+      handler() {
+        products.asyncFind(this.id, item => {
+          this.item = item
+        })
+      }, immediate: true
+    }
   }
 };
 </script>
